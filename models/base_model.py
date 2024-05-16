1bbfc3519d4adfc918be46807a97a2e993c2b216
Rana <rana.alashry6@gmail.com>
2024-05-16 07:53:32 -0700

    5th

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,23 @@ import uuid
 class BaseModel:
     ''' BaseModel class. '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' Constructor. '''
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if (key == "__class__"):
+                    continue
+                elif (key == "created_at" or key == "updated_at"):
+                    setattr(self, key, datetime.strptime(value, t_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         ''' Updates the public instance attribute updated_at. '''
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+''' Basemodel unittest module. '''
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBasemodel(unittest.TestCase):
+    ''' Unittest for basemodel. '''
+
+    def test_init(self):
+        ''' Test for __init__. '''
+
+        model = BaseModel()
+
+        self.assertIsNotNone(model.id)
+        self.assertIsNotNone(model.created_at)
+        self.assertIsNotNone(model.updated_at)
+
+    def test_save(self):
+        ''' Test for save method. '''
+
+        model = BaseModel()
+
+        init_updated_at = model.updated_at
+        cur_updated_at = model.save()
+
+        self.assertNotEqual(init_updated_at, cur_updated_at)
+
+    def test_to_dict(self):
+        ''' Test for to_dict method. '''
+
+        model = BaseModel()
+
+        model_dict = model.to_dict()
+
+        self.assertIsInstance(model_dict, dict)
+
+        self.assertEqual(model_dict["__class__"], 'BaseModel')
+        self.assertEqual(model_dict['id'], model.id)
+        self.assertEqual(model_dict['created_at'],
+                         model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'],
+                         model.updated_at.isoformat())
+
+    def test_str(self):
+        ''' Test for __str__. '''
+
+        model = BaseModel()
+
+        self.assertTrue(str(model).startswith('[BaseModel]'))
+        self.assertIn(model.id, str(model))
+        self.assertIn(str(model.__dict__), str(model))
+
+
+if __name__ == "__main__":
+    unittest.main()
