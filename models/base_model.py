01877dfab62b143702f8ab0070e4a8f314d694ea
Rana <rana.alashry6@gmail.com>
2024-05-18 03:38:09 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,10 @@ class BaseModel:
 
         t_format = "%Y-%m-%dT%H:%M:%S.%f"
 
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
                 if (key == "__class__"):
@@ -22,10 +26,6 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, t_format))
                 else:
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
 
         models.storage.new(self)
 
