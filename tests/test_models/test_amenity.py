2aaa5f1d4641dc0e2f606dd2fd3ca2a86b20fdb3
Rana <rana.alashry6@gmail.com>
2024-05-18 06:00:06 -0700

    amenityTest

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -26,3 +26,16 @@ class TestAmenity_instantiation(unittest.TestCase):
 
     def test_updated_at_public_datetime(self):
         self.assertEqual(datetime, type(Amenity().updated_at))
+
+    def test_name_public_cls_atr(self):
+        amenity = Amenity()
+
+        self.assertEqual(str, type(Amenity.name))
+        self.assertIn("name", dir(Amenity()))
+        self.assertNotIn("name", amenity.__dict__)
+
+    def test_two_amenities_unique_ids(self):
+        amenity1 = Amenity()
+        amenity2 = Amenity()
+
+        self.assertNotEqual(amenity1.id, amenity2.id)
