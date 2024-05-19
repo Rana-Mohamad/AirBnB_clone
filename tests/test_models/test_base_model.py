cd92ee567124f3b3e7f763edc1f44985891288eb
Rana <rana.alashry6@gmail.com>
2024-05-19 06:12:06 -0700

    fixed

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,10 +4,33 @@
 
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
+import models
 
 
 class TestBaseModel(unittest.TestCase):
     ''' Unittest for basemodel. '''
+
+    def test_no_arg_inst(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_new_inst_in_obj(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_two_models_unique_ids(self):
+        model1 = BaseModel()
+        model2 = BaseModel()
+
+        self.assertNotEqual(model1.id, model2.id)
 
     def test_init(self):
         ''' Test for __init__. '''
