a6f500e22198bcaf10ce5718d8dd573384e9a070
Rana <rana.alashry6@gmail.com>
2024-05-19 03:48:53 -0700

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -41,7 +41,7 @@ class TestBasemodel(unittest.TestCase):
         self.assertEqual(model_dict['id'], model.id)
         self.assertEqual(model_dict['created_at'],
                          model.created_at.isoformat())
-        self.assertEqual(model_dict['updated_at'],
+        self.assertEqual(model_dict["updated_at"],
                          model.updated_at.isoformat())
 
     def test_str(self):
