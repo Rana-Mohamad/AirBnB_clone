1b3c7cd5db6e669b3dfc8e4977a4fe08732982f0
Rana <rana.alashry6@gmail.com>
2024-05-20 00:27:31 -0700

    fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -124,6 +124,18 @@ class TestFileStorage_methods(unittest.TestCase):
             self.assertIn("Amenity." + amenity.id, text)
             self.assertIn("Review." + review.id, text)
 
+    def test_save_with_args(self):
+        with self.assertRaises(TypeError):
+            models.storage.save(None)
+
+    def testSaveSelf(self):
+        """ Check save self """
+        msg = "save() takes 1 positional argument but 2 were given"
+        with self.assertRaises(TypeError) as e:
+            FileStorage.save(self, 100)
+
+        self.assertEqual(str(e.exception), msg)
+
     def test_reload(self):
         base_model = BaseModel()
         user = User()
